eb46f680f55d2c4f286f64d17f4d7e858c3dc674
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-04-03 10:56:32 -0500

    🚧 incluir todos los fotogramas

--- src/programa.ts
+++ src/programa.ts
@@ -4,8 +4,8 @@ import vert from './imagenes.vert';
 import frag from './imagenes.frag';
 
 function main() {
-  const canvas = document.getElementById('lienzo') as HTMLCanvasElement;
-  const gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
+  const lienzo = document.getElementById('lienzo') as HTMLCanvasElement;
+  const gl = lienzo.getContext('webgl2') as WebGL2RenderingContext;
 
   const vShader = gl.createShader(gl.VERTEX_SHADER);
   if (!vShader) return;
@@ -37,10 +37,18 @@ function main() {
   const columnas = 6;
   const filas = 4;
   const numCuadros = columnas * filas;
-  const verticesFotogramna = [-0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5];
+  const anchoImg = 4251;
+  const altoImg = 2000;
+  const anchoFotograma = anchoImg / columnas;
+  const altoFotograma = altoImg / filas;
+  lienzo.width = anchoFotograma;
+  lienzo.height = altoFotograma;
+  console.log(anchoFotograma, altoFotograma);
+
+  const verticesFotograma = [-1, -1, 1, -1, -1, 1, 1, 1];
   const vertPositions: number[] = [];
 
-  for (let i = 0; i < numCuadros; i++) vertPositions.push(...verticesFotogramna);
+  for (let i = 0; i < numCuadros; i++) vertPositions.push(...verticesFotograma);
 
   const vertPosVBO = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertPosVBO);
@@ -51,74 +59,38 @@ function main() {
   function crearCoordenadasTextura() {
     const coordenadas = [];
 
-    for (let x = 0; x < columnas; x++) {
-      const x1 = x / columnas;
-      const x2 = (x + 1) / columnas;
-      for (let y = 0; y < filas; y++) {
-        const y1 = y / filas;
-        const cuadro = [x1, y1, x2, y1];
-        coordenadas.push(...cuadro);
+    for (let y = 0; y < filas; y++) {
+      const y1 = y / filas;
+      const y2 = (y + 1) / filas;
+
+      for (let x = 0; x < columnas; x++) {
+        const x1 = x / columnas;
+        const x2 = (x + 1) / columnas;
+        coordenadas.push(...[x1, y2, x2, y2, x1, y1, x2, y1]);
       }
     }
+
+    return new Float32Array(coordenadas);
   }
 
-  const y1 = 1 / filas;
-
-  const texCoords = [
-    // fotograma 00
-    0 / columnas, // x1
-    y1, // y1
-    1 / columnas, // x2
-    y1, // y2
-    0, // x3
-    0, // y3
-    1 / columnas, // x4
-    0, // y4
-    // fotograma 01
-    1 / columnas,
-    y1,
-    2 / columnas,
-    y1,
-    1 / columnas,
-    0,
-    2 / columnas,
-    0,
-    // fotograma 02
-    2 / columnas,
-    y1,
-    3 / columnas,
-    y1,
-    2 / columnas,
-    0,
-    3 / columnas,
-    0,
-    // fotograma 03
-    3 / columnas,
-    y1,
-    1,
-    y1,
-    4 / columnas,
-    0,
-    1,
-    0,
-  ];
   const texCoordVBO = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, texCoordVBO);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, crearCoordenadasTextura(), gl.STATIC_DRAW);
   gl.vertexAttribPointer(aTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(aTexCoordLocation);
 
   const projMatrix = mat4.create();
-  mat4.ortho(projMatrix, 0, 100, 0, 100, 100, -100);
+  const escala = 100;
+  mat4.ortho(projMatrix, 0, escala, 0, escala, escala, -escala);
 
   const viewMatrix = mat4.create();
-  mat4.lookAt(viewMatrix, [0, 0, 90], [0, 0, 0], [0, 1, 0]);
+  mat4.lookAt(viewMatrix, [0, 0, 0], [0, 0, 0], [0, 0, 0]);
 
   const projViewMatrix = mat4.create();
   const mvpMatrix = mat4.create();
   const modelMatrix = mat4.create();
 
-  mat4.mul(projViewMatrix, projMatrix, viewMatrix);
+  mat4.mul(projViewMatrix, projMatrix, mat4.create());
 
   const uMvpMatrixLocation = gl.getUniformLocation(programa, 'uMvpMatrix');
   const uSamplerLocation = gl.getUniformLocation(programa, 'uSampler');
@@ -132,7 +104,7 @@ function main() {
   let lastTime: number;
   let deltaTime: number;
   let animationTime = 0;
-  const timePeriod = 250;
+  const velocidadRep = 100;
   let drawingIndex = 0;
 
   const image = new Image();
@@ -150,7 +122,7 @@ function main() {
     mat4.identity(modelMatrix);
     mat4.translate(modelMatrix, modelMatrix, [50, 50, 0]);
     // mat4.rotateZ(modelMatrix, modelMatrix, 30 * Math. PI / 180);
-    mat4.scale(modelMatrix, modelMatrix, [40, 50, 1]);
+    mat4.scale(modelMatrix, modelMatrix, [50, 50, 1]);
     mat4.mul(mvpMatrix, projViewMatrix, modelMatrix);
     gl.uniformMatrix4fv(uMvpMatrixLocation, false, mvpMatrix);
 
@@ -167,14 +139,14 @@ function main() {
     lastTime = currentTime;
 
     animationTime += deltaTime;
-    if (animationTime >= timePeriod) {
+    if (animationTime >= velocidadRep) {
       animationTime = 0;
 
       gl.clear(gl.COLOR_BUFFER_BIT);
       gl.drawArrays(gl.TRIANGLE_STRIP, drawingIndex, 4);
 
       drawingIndex += 4;
-      if (drawingIndex >= 16) {
+      if (drawingIndex >= 4 * numCuadros) {
         drawingIndex = 0;
       }
     }
